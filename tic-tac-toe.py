8def7ad12aba060b664bc1c8c0b314d063e9c959
spencertyj <avg.irn@gmail.com>
2025-08-24 10:54:01 +0800

    Update tic-tac-toe.py
    win check, draw

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -14,72 +14,78 @@ def draw_board_list(board):
 #End of function
 # list_board = ['1','2','3','4','5','6','7','8','9']
 # draw_board_list(list_board)
-
+# Function to check win condition
+def win_check(board, mark):
+            if board[0] == board[1] == board[2] == mark:
+                return True
+        # to be continued
+def win_check(board, mark):
+    # Check rows
+    if board[0] == board[1] == board[2] == mark:
+        return True
+    if board[3] == board[4] == board[5] == mark:
+        return True
+    if board[6] == board[7] == board[8] == mark:
+        return True
+    # Check columns
+    if board[0] == board[3] == board[6] == mark:
+        return True
+    if board[1] == board[4] == board[7] == mark:
+        return True
+    if board[2] == board[5] == board[8] == mark:
+        return True
+    # Check diagonals
+    if board[0] == board[4] == board[8] == mark:
+        return True
+    if board[2] == board[4] == board[6] == mark:
+        return True
+    return False
 # The main game function
 def game():
     cur_player = 'X'
     list_board = ['1','2','3','4','5','6','7','8','9']
     count = 1
-
-    #repeat 11 - 1 = 10 times because that's the max number of moves a game can have (3x3=9)
-    while count < 11:
+    # repeat 9 times because that's the max number of moves a game can have (3x3=9)
+    while count < 10:
         draw_board_list(list_board)
-
-        # get current player's input
         print("Move ", count, "\n", "Player ", cur_player, " turn. Which square? : ", end="")
         move = input()
-
         if move.isnumeric():
             print("ok, a num")
-
             # Input validation: check if input is a digit between 1 and 9
             if not (1 <= int(move) <= 9):
                 print("Invalid input! Please enter a number between 1 and 9.")
                 continue  # ask for input again
-
         else:
             print("error...")
             continue
-
-        #convert str to int
+        # convert str to int
         move = int(move) - 1
-
-        #check to make sure the cell is not already filled
+        # check to make sure the cell is not already filled
         if list_board[move] != 'X' and list_board[move] != 'O':
             print() #update player's move on the list
-        
-
         else:
             print("That cell is already filled.\nKey in another move?")
             continue
-
-
-        #update player's move on the list
+        # update player's move on the list
         list_board[move] = cur_player
-
-        def win_check(board, mark):
-            if board[0] == board[1] == board[2] == mark:
-                return True
-        # to be continued
-
-        #switch the current player
-        if cur_player == 'X':
-            cur_player = 'O'
-        else:
-            cur_player = 'X'
-        
-        count = count + 1  
-
-
-
+        # Check for win after each move
+        if win_check(list_board, cur_player):
+            draw_board_list(list_board)
+            print(f"Player {cur_player} wins!")
+            return  # End the game
+        # switch the current player
+        cur_player = 'O' if cur_player == 'X' else 'X'
+        count += 1
+    # If no winner after 9 moves
+    draw_board_list(list_board)
+    print("It's a draw!")
 #main
 game()
-
 # # Function to print the score-board
 # def print_scoreboard(score_board):
 #     print("--------------------------------")
 #     print('         SCOREBOARD         ')
-#     print("--------------------------------")
-
+#     print("--------------------------------")2
 #     players = list(score_board.keys())
 #     print("  ", players[0], "  ",)
